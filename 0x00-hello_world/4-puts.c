71a8a16fcdff33f9c8009d072da3701c7de8a77a
Daniel <omosefunmid@gmail.com>
2021-08-27 07:32:34 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 #include <stdio.h>
    /**
     * main - main block
@@ -6,6 +6,6 @@
     */
 int main(void)
 {
-  puts( "\" Programming is like building a multilingual puzzle");
-  return (0);
+puts( "\" Programming is like building a multilingual puzzle");
+return (0);
 }
